7aec9c06ace076d645e9366e945c6378d8d9eaa5
moling <365024424@qq.com>
2018-03-16 14:20:30 +0800

    导入Element-UI

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './App'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
