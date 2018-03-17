e75e6db7fdfacccc5395a0756072344be1475c3d
moling <365024424@qq.com>
2018-03-17 18:53:59 +0800

    添加ZTable组件

---
+++ src/components/ZTable.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="z-table">
+    <div class="table-wrap">
+      <el-table v-loading="loading"
+        :data="tableData" border stripe size="mini"
+        :header-cell-style="{backgroundColor: '#409eff', color: '#fff'}"
+        element-loading-text="拼命加载中"
+        element-loading-spinner="el-icon-loading"
+        element-loading-background="rgba(0, 0, 0, 0.8)"
+        style="width: 100%"
+        tooltip-effect="dark"
+      >
+        <el-table-column v-for="field in fields" :key="field" :prop="field" :label="field" show-overflow-tooltip/>
+      </el-table>
+    </div>
+    <el-pagination background layout="prev, pager, next" :total="data.length" :page-size="pageSize" :current-page.sync="index"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ZTable',
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    tableData () {
+      return this.data.slice((this.index - 1) * this.pageSize, this.index * this.pageSize)
+    },
+    fields () {
+      if (!this.data.length) {
+        return []
+      }
+      return Object.keys(this.data[0])
+    }
+  },
+  data () {
+    return {
+      pageSize: 5,
+      index: 0
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.table-wrap {
+  height: 300px;
+}
+.cell {
+  margin: 5px 0;
+}
+</style>
