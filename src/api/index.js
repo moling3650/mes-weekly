3c3cd852f6d5c26495705f85aaa8c4c03d68897d
moling <365024424@qq.com>
2018-03-19 10:03:38 +0800

    去除日期的时分秒

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,11 @@ function fetchProcessOptions () {
 }
 function fetchWeeklyData (processCode, startDay, endDay) {
   const params = `processCode=${processCode}&startDate=${startDay}&endDate=${endDay}`
-  return axios.get(`/DataAPI/Commom.ashx?ActType=GetWeeklyData&${params}`).then(res => res.data)
+  return axios.get(`/DataAPI/Commom.ashx?ActType=GetWeeklyData&${params}`)
+    .then(res => res.data.map(item => {
+      item['日期'] = item['日期'].split(' ')[0]
+      return item
+    }))
 }
 
 export {
